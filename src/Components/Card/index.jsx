13fd66e81c5c81cbe27fa330df34552bc7747997
WDRoa <willieroa01@gmail.com>
2024-01-13 05:27:05 -0400

    Was initiated the creation of product details and the implementation of icons.

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
@@ -10,7 +11,7 @@ const Card = (data) => {
           <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category}</span>
           <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
           <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={() => context.setCount(context.count + 1)}>
-            +
+          <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
           </div>
         </figure>
         <p className='flex justify-between'>
