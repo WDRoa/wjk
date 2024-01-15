54b1f3fa894ba3b4c87e7b487f4bc28e2cb0a891
WDRoa <willieroa01@gmail.com>
2024-01-15 10:52:44 -0400

    In the shopping cart, duplicate products were avoided.

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -10,7 +10,7 @@ const CheckoutSideMenu = () => {
 
   return (
     <aside
-      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+      className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-1 border border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>My Order</h2>
         <div>
@@ -19,7 +19,7 @@ const CheckoutSideMenu = () => {
             onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
       </div>
-      <div className='px-6'>
+      <div className='px-6 overflow-y-scroll'>
         {
           context.cartProducts.map(product => (
             <OrderCard
@@ -36,3 +36,5 @@ const CheckoutSideMenu = () => {
 }
 
 export default CheckoutSideMenu
+
+
